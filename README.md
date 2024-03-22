97dd5f95e782358b1daf874325207b3ba5654a28
xzgao <xz.gao@connect.ust.hk>
2024-03-22 19:33:20 +0800

    update figs

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Benchmark for various ways to calculate polynomials
 
 Compare the performance of Polynomials.jl and Estrin.jl for calculating polynomials.
 
-![](./scripts/out/XZDesktop/2024-03-22--19-07-52/estrin_vs_horner_vs_polynomial.png)
+![](./scripts/out/XZDesktop/2024-03-22--19-07-52/estrin_vs_horner_vs_polynomial_Float32.png)
+
+![](./scripts/out/XZDesktop/2024-03-22--19-07-52/estrin_vs_horner_vs_polynomial_Float64.png)
--- scripts/generate_figs.jl
+++ scripts/generate_figs.jl
@@ -17,15 +17,17 @@ function generate_fig()
     
     parsed_args = parse_commandline()
     in_file = parsed_args["in-file"]
-
+    
     df0 = CSV.read(in_file, DataFrame)
-    df = filter(row -> row.T == "Float64", df0)
-    plot(xlabel = "log2(n)", ylabel = "log10(t)", title = "Estrin vs Horner vs Polynomials.jl (Float64)", legend = :topleft, dpi = 500, xlim = [minimum(df.n) - 1, maximum(df.n) + 1])
-    plot!(df.n, log10.(df.et), label = "Estrin", marker = :circle)
-    plot!(df.n, log10.(df.ht), label = "Horner", marker = :square)
-    plot!(df.n, log10.(df.pt), label = "Polynomials.jl", marker = :diamond)
+    for T0 in ["Float32", "Float64"]
+        df = filter(row -> row.T == T0, df0)
+        plot(xlabel = "log2(n)", ylabel = "log10(t)", title = "Estrin vs Horner vs Polynomials.jl ($T0)", legend = :topleft, dpi = 500, xlim = [minimum(df.n) - 1, maximum(df.n) + 1])
+        plot!(df.n, log10.(df.et), label = "Estrin", marker = :circle)
+        plot!(df.n, log10.(df.ht), label = "Horner", marker = :square)
+        plot!(df.n, log10.(df.pt), label = "Polynomials.jl", marker = :diamond)
 
-    savefig("estrin_vs_horner_vs_polynomial.png")
+        savefig("estrin_vs_horner_vs_polynomial_$T0.png")
+    end
 
     nothing
 end
